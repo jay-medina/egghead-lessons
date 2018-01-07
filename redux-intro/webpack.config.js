a3b51050d48ee0c420f9d1081bf2b1fa95ec3399
Jose Medina <josemedina921@gmail.com>
2018-01-06 20:34:43 -0800

    initial files

---
+++ redux-intro/webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  entry: {
+    app: "./app/main.tsx"
+  },
+  output: {
+    path: "dist",
+    filename: "index.js"
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  },
+  devServer: {
+    inline: true,
+    port: 8080
+  },
+  devtool: "source-map",
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "ts-loader"
+      }
+    ],
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  }
+};
