9bd79223d6e38fbd64263335c5e7552e93f64e67
Jose Medina <josemedina921@gmail.com>
2018-03-24 10:26:29 -0700

    updating the main tsx

--- elm_web_app/src/lesson7/Main.tsx
+++ elm_web_app/src/lesson7/Main.tsx
@@ -1,33 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-interface Ship {
-  name: string;
-  cost: number;
-}
+type PrintThing = <T>(thing: T) => JSX.Element;
 
-const ships = [
-  { name: 'X-wing', cost: 149999 },
-  { name: 'Millennium Falcon', cost: 100000 },
-  { name: 'Death Star', cost: 1000000000000 }
-];
+const numbers = [1, 2, 3, 4, 5];
 
-const renderShip: React.SFC<Ship> = ship => (
-  <li>
-    <span>{ship.name}</span>
-    <span>,</span>
-    <b>{ship.cost}</b>
-  </li>
-);
+const fruits = [{ name: 'Orange' }, { name: 'Bananas' }];
 
-const RenderShips = () => (
-  <div style={{ padding: '1em' }}>
-    <h1>Ships</h1>
-    <ul>{ships.map(renderShip)}</ul>
-  </div>
-);
+const printThing: PrintThing = thing => <div>{thing}</div>;
 
 function main() {
-  const mainDom = document.getElementById('#main');
-  ReactDOM.render(<RenderShips />, mainDom);
+  const mainDom = document.getElementById('#root');
+  ReactDOM.render(printThing(fruits), mainDom);
 }
+
+main();
