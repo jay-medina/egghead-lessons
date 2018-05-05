73606b7e6e37b9977965dc84514f2276721620d0
Jose Medina <josemedina921@gmail.com>
2018-05-05 09:45:15 -0700

    adding component Did catch

--- react16_features/src/lesson1/index.tsx
+++ react16_features/src/lesson1/index.tsx
@@ -1,7 +1,47 @@
 import React from 'react';
 
-const App = () => {
-  return <div>hello world</div>;
+type User = {
+  name: string;
 };
+
+const Profile = ({ user }: { user: User | null }) => (
+  <div>Name: {user!.name}</div>
+);
+
+interface AppState {
+  user: User | null;
+  hasError: boolean;
+}
+
+class App extends React.Component<{}, AppState> {
+  constructor(props = {}) {
+    super(props);
+    this.state = {
+      user: { name: 'Jose' },
+      hasError: false
+    };
+  }
+
+  componentDidCatch() {
+    this.setState(state => ({ ...state, hasError: true }));
+  }
+
+  updateUser = () => {
+    this.setState(state => ({ ...state, user: null }));
+  };
+
+  render() {
+    if (this.state.hasError) {
+      return 'Sorry, something went wrong';
+    }
+
+    return (
+      <div>
+        <Profile user={this.state.user} />
+        <button onClick={this.updateUser}>Update</button>
+      </div>
+    );
+  }
+}
 
 export default App;
