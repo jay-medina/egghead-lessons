6256c9639a44de2ad85053afafc93c332235b78d
Jose Medina <josemedina921@gmail.com>
2018-05-07 21:33:27 -0700

    finish lesson2 (#41)

--- react16_features/src/index.tsx
+++ react16_features/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './lesson1';
+// import App from './lesson1';
+import App from './lesson2';
 
 ReactDOM.render(<App />, document.getElementById('root'));
