cd5ae34421924742dc81cd0c530f18f26505c1eb
Jose Medina <josemedina921@gmail.com>
2018-05-13 22:03:37 -0700

    showing how to prevent async changes

--- react16_features/src/index.tsx
+++ react16_features/src/index.tsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 // import App from './lesson2';
 // import App from './lesson3';
 // import App from './lesson4';
-import App from './lesson5';
+// import App from './lesson5';
+import App from './lesson6';
 
 ReactDOM.render(<App />, document.getElementById('root'));
